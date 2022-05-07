c11abf4b3499ab9a9efe30f3df9ec1a6efca8a34
ralphdosan <ralphvincent.pagcaliwagan@yahoo.com>
2022-05-07 23:46:34 +0800

    update

--- db/loaning_act.sql
+++ db/loaning_act.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2022 at 04:57 PM
+-- Generation Time: May 07, 2022 at 05:42 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -43,8 +43,13 @@ INSERT INTO `account` (`user_id`, `email_address`, `password`, `date_created`) V
 (16, 'ralphvincent@yahoo.com', '123', '2022-05-03 09:39:56'),
 (17, 'chadwick@yahoo.com', '12345', '2022-05-03 09:43:39'),
 (18, 'richard.ramos@yahoo.com', '123', '2022-05-03 09:45:05'),
-(19, 'chadwick@yahoo.com', '123', '2022-05-04 03:38:20'),
-(20, 'neil.pornela@yahoo.com', '123', '2022-05-04 05:26:02');
+(19, 'chadwick@yahoo.com', '123aaa', '2022-05-07 07:37:33'),
+(20, 'neil.pornela@yahoo.com', '123', '2022-05-04 05:26:02'),
+(21, 'chadwick@yahoo.com', '123', '2022-05-07 06:24:03'),
+(22, 'johnllyod@yahoo.com', '1213', '2022-05-07 06:27:40'),
+(23, '', '123', '2022-05-07 15:23:49'),
+(24, 'johnwick@yahoo.com', '123', '2022-05-07 15:26:35'),
+(25, 'nimezmaricito@yahoo.com', '123', '2022-05-07 15:37:08');
 
 -- --------------------------------------------------------
 
@@ -81,16 +86,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`customer_id`, `lname`, `fname`, `mname`, `category`, `permanent_address`, `email`, `date_birth`, `mobile_no`, `id_number`, `mother_name`, `mother_no`, `father_name`, `father_no`, `spouse_name`, `spouse_no`, `company_affiliated`, `company_address`, `company_no`, `company_position`, `company_status`) VALUES
-(1, 'pagcaliwagan', 'ralph vincent', 'solis', '', 'hanggan 1 Calauan Laguna', 'ralphvincent.pagcaliwagan@yahoo.com', '0000-00-00', 98765, 123, 'edna pagcaliwagan', 12345, 'ariel pagcaliwagan', 12345, 'ralph vincent pagcaliwagan', 12345, 'dsadadasd', 'asdasdsadasd', 12345, '0', 'manager'),
-(2, 'pagcaliwagan', 'ralph vincent', 'solis', '', 'hanggan 1 Calauan Laguna', 'ralphvincent.pagcaliwagan@yahoo.com', '2001-11-08', 98765, 123, 'edna pagcaliwagan', 12345, 'ariel pagcaliwagan', 12345, 'ralph vincent pagcaliwagan', 12345, 'dsadadasd', 'asdasdsadasd', 12345, '', 'manager'),
-(3, 'pagcaliwagan', 'ralph vincent', 'solis', '', 'hanggan 1 Calauan Laguna', 'ralphvincent.pagcaliwagan@yahoo.com', '2001-11-08', 98765, 123, 'edna pagcaliwagan', 12345, 'ariel pagcaliwagan', 12345, 'ralph vincent pagcaliwagan', 12345, 'dsadadasd', 'asdasdsadasd', 12345, '', 'manager'),
-(4, 'pagcaliwagan', 'ralph vincent', 'solis', '', 'hanggan 1 Calauan Laguna', 'ralphvincent.pagcaliwagan@yahoo.com', '2001-11-08', 98765, 123, 'edna pagcaliwagan', 12345, 'ariel pagcaliwagan', 12345, 'ralph vincent pagcaliwagan', 12345, 'dsadadasd', 'asdasdsadasd', 12345, '', 'manager'),
-(5, 'Ramos', 'richard', 'sadsads', '', 'hanggan 1 Calauan Laguna', 'richard.ramos@yahoo.com', '1222-02-08', 98765, 123, 'sadadadasdada', 123124, 'qweqeqewq', 123123, 'asdasdasdasdsa', 12345, 'dsadadasd', 'asdasdsadasd', 12345, '', 'manager'),
-(6, 'Ramos', 'richard', 'sadsads', '', 'hanggan 1 Calauan Laguna', 'richard.ramos@yahoo.com', '1222-02-08', 98765, 123, 'sadadadasdada', 123124, 'qweqeqewq', 123123, 'asdasdasdasdsa', 12345, 'dsadadasd', 'asdasdsadasd', 12345, 'manager', 'manager'),
-(7, 'porn', 'Jireh', 'sadsads', 'atm', 'hanggan 1 Calauan Laguna', 'richard.ramos@yahoo.com', '2222-02-02', 98765, 123, 'adasdasdsad', 12345, 'sadasdasdasd', 1234, 'asdasdasdsadas', 12345, 'dsadadasd', 'asdasdsadasd', 12345, 'manager', 'manager'),
-(8, 'porn', 'Jireh', 'sadsads', 'atm', 'hanggan 1 Calauan Laguna', 'richard.ramos@yahoo.com', '2222-02-02', 98765, 123, 'adasdasdsad', 12345, 'sadasdasdasd', 1234, 'asdasdasdsadas', 12345, 'dsadadasd', 'asdasdsadasd', 12345, 'manager', 'manager'),
-(9, 'porn', 'Jireh', 'solis', 'sps', 'hanggan 1 Calauan Laguna', 'ralphvincent.pagcaliwagan@yahoo.com', '1111-01-01', 98765, 123, 'sdaddsadasdas', 12345, 'sadasdadsad', 123123, 'dadasdadsad', 1234, 'dsadadasd', 'asdasdsadasd', 12345, 'manager', 'manager'),
-(10, 'pornela', 'neil arthur', 'saaa', 'atm', 'hanggan 1 Calauan Laguna', 'Neilarthur23@yahoo.com', '2001-01-01', 98765, 123, 'sadadadasdada', 123124, 'qweqeqewq', 12345, 'asdasdasdasdsa', 12345, 'dsadadasd', 'asdasdsadasd', 12345, 'manager', 'manager');
+(6, 'Ramos', 'richard', 'sadsads', 'sps', 'hanggan 1 Calauan Laguna', 'richard.ramos@yahoo.com', '1222-02-08', 98765, 123, 'sadadadasdada', 123124, 'qweqeqewq', 123123, 'asdasdasdasdsa', 12345, 'dsadadasd', 'asdasdsadasd', 12345, 'manager', 'manager'),
+(11, 'pagcaliwagan', 'charles vincent', 'solis', 'atm', 'hanggan 1 Calauan Laguna', 'charlespagcaliwagan@yahoo.com', '2001-11-08', 9475632, 881, 'edna pagcaliwagan', 947563219, 'ariel pagcaliwagan', 2147483647, 'ralph vincent pagcaliwagan', 94534212, 'samsan tech', 'calauan laguna', 987654321, 'manager wasss', 'magssss');
 
 -- --------------------------------------------------------
 
@@ -115,6 +112,15 @@ CREATE TABLE `loan_info` (
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `Lastname` varchar(255) NOT NULL,
+  `mid_name` varchar(255) NOT NULL,
+  `p_address` varchar(255) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `t_number` int(11) NOT NULL,
+  `educational` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `civil_status` varchar(255) NOT NULL,
   `position` enum('admin','branch manager','staff') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -122,12 +128,17 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `name`, `position`) VALUES
-(16, 'ralphvincent', 'admin'),
-(17, 'chadwick balota', 'admin'),
-(18, 'richard ramos', 'admin'),
-(19, 'no name', 'branch manager'),
-(20, 'neil pornela', 'staff');
+INSERT INTO `user` (`user_id`, `name`, `Lastname`, `mid_name`, `p_address`, `birthday`, `t_number`, `educational`, `age`, `gender`, `civil_status`, `position`) VALUES
+(16, 'ralphvincent', '', '', '', NULL, 0, '', 0, '', '', 'admin'),
+(17, 'chadwick balota', '', '', '', NULL, 0, '', 0, '', '', 'admin'),
+(18, 'richard ramos', '', '', '', NULL, 0, '', 0, '', '', 'admin'),
+(19, 'no name', '', '', '', NULL, 0, '', 0, '', '', 'branch manager'),
+(20, 'neil pornela', '', '', '', NULL, 0, '', 0, '', '', 'staff'),
+(21, 'charlesvincent', '', '', '', NULL, 0, '', 0, '', '', 'admin'),
+(22, 'john llyold', '', '', '', NULL, 0, '', 0, '', '', 'admin'),
+(23, 'justin', 'violanta', 'dale', 'calauan laguna', '2001-02-01', 94726352, 'information technology', 21, 'male', 'single', 'staff'),
+(24, 'john', 'balota', 'wick', 'calauan laguna', '2001-02-02', 49582211, 'information technology', 21, 'Male', 'single', 'branch manager'),
+(25, 'nimez', 'Maricito', 'haha', 'calauan laguna', '2001-02-01', 986543, 'information technology', 21, 'Male', 'single', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -166,13 +177,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `loan_info`
